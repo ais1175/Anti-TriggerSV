bff6b52352150ed5c9cb6345c540605ed0aeb1b5
uknowxd <kitisaksrisuk@gmail.com>
2023-01-13 16:10:33 +0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'adamant'
 games { 'gta5' }
 
 author 'uknowxd#5513'
-description 'Anti-TriggerSV By: Qwerty-Hub'
+description 'Anti-TriggerSV By: Qwerty'
 
 files {	'qwerty-protection/**' }
 server_scripts { 'config.lua' , 'core/sv.lua' }
